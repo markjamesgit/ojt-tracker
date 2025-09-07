4a27cdb084d561d4ddd47dd7086ce8db76c5a554
Mark James Villagonzalo <142849183+markjamesgit@users.noreply.github.com>
2025-09-07 18:17:12 +0800

    feat: stop tracking .env file

---
+++ src/firebase/index.js
@@ -0,0 +1,26 @@
+//Import the SDK
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
+
+//Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+}
+
+//Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+//initialize services
+const auth = getAuth(app)
+const db = getFirestore(app)
+const storage = getStorage(app)
+
+//Export to use in Vue components
+export { auth, db, storage }
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,39 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    />
+  <q-page class="q-pa-md">
+    <h1>OJT Tracker Test</h1>
+
+    <div v-if="students.length">
+      <q-list bordered separator>
+        <q-item v-for="student in students" :key="student.id">
+          <q-item-section> {{ student.name }}</q-item-section>
+          <q-item-section side> {{ student.hours }} hours</q-item-section>
+        </q-item>
+      </q-list>
+    </div>
+
+    <div v-else>
+      <q-banner class="bg-grey-3 text-dark q-mt-md"> No students found in Firebase. </q-banner>
+    </div>
   </q-page>
 </template>
 
 <script setup>
-//
+import { ref, onMounted } from 'vue'
+import { db } from 'src/firebase/index'
+import { collection, getDocs } from 'firebase/firestore'
+
+const students = ref([])
+
+onMounted(async () => {
+  try {
+    const snapshot = await getDocs(collection(db, 'students'))
+    students.value = snapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }))
+    console.log('Fetched students: ', students.value)
+  } catch (error) {
+    console.error('Error fetching students: ', error)
+  }
+})
 </script>
